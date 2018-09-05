3c3b47c1173b558457099bb3326b1e53ea4e877e
backendupdate <desarrolloweb@update.pe>
2018-09-05 18:02:50 -0500

    producto

--- producto-detalle.php
+++ producto-detalle.php
@@ -142,78 +142,83 @@
                             <div class="tab-content">
                                 <div role="tabpanel" class="tab-pane active" id="home">
                                     <div class="single-service">
-                                      <div class_="col-md-12">
-                                        <h2><?php echo $xTitulo; ?></h2>
-                                    </div>
-
-                                        <div class_="col-md-12">
-                                        <div class="col-md-6">
-                                            <img src="/cms/assets/img/productos/<?php echo $xImagen; ?>" alt="<?php echo $xTitulo; ?>">
+                                        <div class="row">
+                                            <div class_="col-md-12">
+                                                <h2><?php echo $xTitulo; ?></h2>
+                                            </div><!--fin de div class col-md-12-->
                                         </div>
-                                        <div class="col-md-6">
-                                            <?php echo $xDescripcion; ?>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <img src="/cms/assets/img/productos/<?php echo $xImagen; ?>" alt="<?php echo $xTitulo; ?>">
+                                            </div>
+                                            <div class="col-md-6">
+                                                <?php echo $xDescripcion; ?>
+                                            </div>
                                         </div>
-                                        <div class="col-md-12">
-                                            <?php echo $xDescripcion2; ?>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <?php echo $xDescripcion2; ?>
+                                            </div>
                                         </div>
-                                       </div>
-
+                                        <div class="row">
                                         <?php if($estado_tab==1){ ?>
-                                        <h4><?php echo $subtitulo; ?></h4>
-                                        <div class="about-inner-page">
-                                            <!-- Nav tabs -->
-                                            <ul class="nav nav-tabs" role="tablist">
-                                                <?php if($titulo_a==""){ }else{ ?>
-                                                <li role="presentation" class="active"><a href="#taba" aria-controls="taba" role="tab" data-toggle="tab"><?php echo $titulo_a; ?></a></li>
-                                                <?php } ?>
-                                                <?php if($titulo_b==""){ }else{ ?>
-                                                <li role="presentation"><a href="#tabb" aria-controls="tabb" role="tab" data-toggle="tab"><?php echo $titulo_b; ?></a></li>
-                                                <?php } ?>
-                                                <?php if($titulo_c==""){ }else{ ?>
-                                                <li role="presentation"><a href="#tabc" aria-controls="tabc" role="tab" data-toggle="tab"><?php echo $titulo_c; ?></a></li>
-                                                <?php } ?>
-                                            </ul>
-                                            <!-- Tab panes -->
-                                            <div class="tab-content">
-                                                <?php if($contenidos_a==""){ }else{ ?>
-                                                <div role="tabpanel" class="tab-pane active" id="taba" onclick="myFunction()">
-                                                    <div class="media contenido_media">
-                                                        <div class="media-body" id="titulo_a">
-                                                            <h5><?php echo $titulo_a; ?>  
-                                                            </h5>
-                                                            <?php echo $contenidos_a; ?>
+                                        <div class="col-md-12">
+                                            <h4><?php echo $subtitulo; ?></h4>
+                                            <div class="about-inner-page">
+                                                <!-- Nav tabs -->
+                                                <ul class="nav nav-tabs" role="tablist">
+                                                    <?php if($titulo_a==""){ }else{ ?>
+                                                    <li role="presentation" class="active"><a href="#taba" aria-controls="taba" role="tab" data-toggle="tab"><?php echo $titulo_a; ?></a></li>
+                                                    <?php } ?>
+                                                    <?php if($titulo_b==""){ }else{ ?>
+                                                    <li role="presentation"><a href="#tabb" aria-controls="tabb" role="tab" data-toggle="tab"><?php echo $titulo_b; ?></a></li>
+                                                    <?php } ?>
+                                                    <?php if($titulo_c==""){ }else{ ?>
+                                                    <li role="presentation"><a href="#tabc" aria-controls="tabc" role="tab" data-toggle="tab"><?php echo $titulo_c; ?></a></li>
+                                                    <?php } ?>
+                                                </ul>
+                                                <!-- Tab panes -->
+                                                <div class="tab-content">
+                                                    <?php if($contenidos_a==""){ }else{ ?>
+                                                    <div role="tabpanel" class="tab-pane active" id="taba" onclick="myFunction()">
+                                                        <div class="media contenido_media">
+                                                            <div class="media-body" id="titulo_a">
+                                                                
+                                                                <?php echo $contenidos_a; ?>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <?php } ?>
-                                                <?php if($contenidos_b==""){ }else{ ?>
-                                                <div role="tabpanel" class="tab-pane" id="tabb" onclick="myFunction2()">
-                                                    <div class="media contenido_media">
-                                                        <div class="media-body" id="titulo_b">
-                                                            <h5><?php echo $titulo_b; ?></h5>
-                                                            <?php echo $contenidos_b; ?>
+                                                    <?php } ?>
+                                                    <?php if($contenidos_b==""){ }else{ ?>
+                                                    <div role="tabpanel" class="tab-pane" id="tabb" onclick="myFunction2()">
+                                                        <div class="media contenido_media">
+                                                            <div class="media-body" id="titulo_b">
+                                                                
+                                                                <?php echo $contenidos_b; ?>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <?php } ?>
-                                                <?php if($contenidos_c==""){ }else{ ?>
-                                                <div role="tabpanel" class="tab-pane" id="tabc">
-                                                    <div class="media contenido_media" id="titulo_c">
-                                                        <div class="media-body">
-                                                            <h5><?php echo $titulo_c; ?></h5>
-                                                            <?php echo $contenidos_c; ?>
+                                                    <?php } ?>
+                                                    <?php if($contenidos_c==""){ }else{ ?>
+                                                    <div role="tabpanel" class="tab-pane" id="tabc">
+                                                        <div class="media contenido_media" id="titulo_c">
+                                                            <div class="media-body">
+                                                               
+                                                                <?php echo $contenidos_c; ?>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <?php } ?>
-                                            </div><!--Fin div tab panes-->
-                                        </div><!--fin div about-inner-page-->
-                                        <?php } ?>
+                                                    <?php } ?>
+                                                </div><!--Fin div tab panes-->
+                                            </div><!--fin div about-inner-page-->
+                                            <?php } ?>
 
-                                        <!-- Go to www.addthis.com/dashboard to customize your tools -->
-                                        <div class="addthis_inline_share_toolbox"></div>
-                                        <!-- Go to www.addthis.com/dashboard to customize your tools -->
-                                        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5ae1e6fc57e4c84d"></script>
+                                            <!-- Go to www.addthis.com/dashboard to customize your tools -->
+                                            <div class="addthis_inline_share_toolbox"></div>
+                                            <!-- Go to www.addthis.com/dashboard to customize your tools -->
+                                            <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5ae1e6fc57e4c84d"></script>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>            
                             </div>
